4f6677bab02ea04870a8729e31545e0c20272602
sowoi <10256263+sowoi@users.noreply.github.com>
2022-09-29 13:12:13 +0200

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+# Developer: Massoud Ahmed
+
+# create temp Dir
+DIR=`mktemp -d`
+CUR=$PWD
+cd $DIR
+# Paperless NGX consume path
+OUTPUT="/srv/scans"
+
+# get scanner info 
+scanner=`scanimage -L | cut -d " " -f 2 | sed 's/\`//'  | sed "s/'//" ` 
+scanimage -b --format png  -d "$scanner"  --source "Automatic Document Feeder(center aligned,Duplex"  --resolution 200 --AutoDocumentSize="yes" -v
+
+
+# check for blanks
+echo "$?"
+if [ $? -eq 0 ]
+   then
+
+COUNTER=0
+for f in *.png;
+do
+    
+    if [[ `identify -verbose $f | grep "skewness" | tail -n 1 | cut -d ":" -f 2 | cut -d "." -f 1` -lt -6 ]]; then
+        #check if png is blank and remove it
+        if [[ $COUNTER -eq 1 ]]; then
+            # save the first page
+            echo "$f will not be removed"
+            continue
+        else
+         echo "$f will be removed"
+         rm $f
+        fi
+    else
+        #else keep the png
+        echo "$f will not be removed"
+        continue
+    fi;
+    COUNTER=$((COUNTER+1))
+           
+           
+done
+
+
+# reverse order of png and convert to pdf with rotation
+filesInFolder=`ls -rv`
+echo "$filesInFolder"
+convert $filesInFolder -rotate 180 $CUR/$1
+
+cd $CUR
+
+# move them to scans folder
+mv $1 "$OUTPUT"
+if [ $? -eq 1 ]; then
+   exit 1
+fi
+echo "$OUTPUT/$1"
+exit 0
+
+else
+    exit 1
+fi 
